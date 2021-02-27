c4ed8216a8e88e5b445980c978722691127559f6
Weff182 <weffomax@gmail.com>
2021-02-27 18:48:08 +0300

    feat: added new functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  str.split('');
+  let stak = [];
+  let open = ['(', '{', '[', '|'];
+  let closed = [')', '}', ']', '|'];
+  let openIndex;
+  let closedIndex;
+  let palka;
+  
+  for (let i = 0; i < str.length; i++){
+     openIndex = open.indexOf(str[i]);
+     palka = stak.indexOf('|');
+     if (openIndex !== -1){
+        if (openIndex !== '|'){
+          if (palka !== -1){
+            if (palka !== stak[i-1]){
+              return false;
+            }
+            stak.push(palka);
+          }
+        stak.push(openIndex);
+     }
+     }  
+      closedIndex = closed.indexOf(str[i]);
+      if (closedIndex !== -1){
+        openIndex = stak.pop();
+      if (openIndex !== closedIndex){
+          return false;
+      } 
+      }
+    }
+  if(stak.length == 0){
+          return true;
+      } else {
+          return false;
+      }
 }
+
+
